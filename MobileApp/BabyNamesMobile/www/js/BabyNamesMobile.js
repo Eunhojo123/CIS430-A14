440c2bc406345fac2fca33d38bd8e6db8fcc4d0c
Eunhojo123 <0718joeunho@gmail.com>
2023-11-19 02:35:57 -0700

    CSS RWD

---
+++ MobileApp/BabyNamesMobile/www/js/BabyNamesMobile.js
@@ -0,0 +1,71 @@
+"use strict";
+        
+// default query values
+let state       = "AL"
+let sex         = "girl"
+let year        = "1960"
+let queryString = ""
+
+if (window.cordova) {
+    /* will only be true of cordova.js is loaded, which means running on phone
+    /* wait until cordova is loaded, it will call onDeviceReady*/
+    document.addEventListener("deviceready", initialize, false);
+} else {
+    // running in a browser to test/develop
+    initialize();
+}
+
+function initialize() {
+    updateQueryString()
+}    
+
+function updateState(value) { 
+    state = value 
+    updateQueryString()
+}
+
+function updateSex(value)   { 
+    sex = value 
+    updateQueryString()
+}
+
+function updateYear(value)  { 
+    year = value 
+    updateQueryString()
+}
+
+function updateQueryString() {
+    queryString = 
+
+      "SELECT name, number, state, sex, year \n" 
+    + "FROM   NamesNumberByStateYear \n"
+    + "WHERE\n "
+    + " state = "    + "'" + state   + "'"
+    + " AND sex = "  + "'" + sex     + "'"
+    + " AND year = " + "'" + year    + "'\n"
+    + "ORDER BY number DESC LIMIT 5;"
+    document.getElementById('queryStingId').innerHTML = queryString;
+}
+
+function runQuery() {
+    MySql.Execute(
+        "sql.wpc-is.online",	// mySQL server
+        "demo", 				// login name
+        "demo12345", 			// login password
+        "BabyNames", 			// database to use
+                                // SQL query string:
+        queryString,
+        function (data) {
+            processQueryResult(data);
+        }
+    );
+}
+
+function processQueryResult(queryReturned) {
+    if (!queryReturned.Success) {
+        alert(queryReturned.Error)
+    } else {
+        document.getElementById("output").innerHTML = 
+            JSON.stringify(queryReturned.Result, null, 2);
+    }
+}
